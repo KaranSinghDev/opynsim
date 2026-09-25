b22d0b2a1221ba1fae015362d5dac818fe356f16
Adam Kewley <contact@adamkewley.com>
2026-09-25 11:31:28 +0200

    Clean up/clarify reinterpret_cast usage in SharedPreHashedString (#36).

--- liboscar/utilities/shared_pre_hashed_string.h
+++ liboscar/utilities/shared_pre_hashed_string.h
@@ -68,42 +68,22 @@ namespace osc
         explicit SharedPreHashedString() :
             SharedPreHashedString{static_default_instance()}
         {}
-
-        explicit SharedPreHashedString(std::string_view str)
-        {
-            const size_t num_bytes_allocated =
-                sizeof(Metadata) +               // metadata
-                sizeof(value_type)*str.size() +  // character data
-                sizeof(value_type);              // NUL terminator
-
-            ptr_ = ::operator new(num_bytes_allocated, std::align_val_t{alignof(Metadata)});
-
-            // initialize `Metadata` at bytes `[0, sizeof(Metadata))`
-            static_assert(std::is_nothrow_constructible_v<Metadata, decltype(str)>);
-            new (ptr_) Metadata{str};
-
-            // initialize character data at `[sizeof(Metadata), sizeof(Metadata) + str.size())`
-            static_assert(alignof(Metadata) >= alignof(value_type));
-            static_assert(std::is_nothrow_copy_constructible_v<value_type>);
-            auto* character_data_ptr = std::launder(reinterpret_cast<value_type*>(static_cast<std::byte*>(ptr_) + sizeof(Metadata)));
-            std::uninitialized_copy_n(str.data(), str.size(), character_data_ptr);
-
-            // initialize NUL terminator at `sizeof(Metadata) + str.size()`
-            character_data_ptr[str.size()] = value_type{};
-        }
+        explicit SharedPreHashedString(std::string_view str) :
+            storage_{Storage::create(str)}
+        {}
         explicit SharedPreHashedString(const char* s) : SharedPreHashedString{std::string_view{s}} {}
         explicit SharedPreHashedString(std::nullptr_t) = delete;
 
         SharedPreHashedString(const SharedPreHashedString& src) noexcept :
-            ptr_{src.ptr_}
+            storage_{src.storage_}
         {
-            static_cast<Metadata*>(ptr_)->num_owners.fetch_add(1, std::memory_order_relaxed);
+            storage_->num_owners.fetch_add(1, std::memory_order_relaxed);
         }
 
         SharedPreHashedString(SharedPreHashedString&& tmp) noexcept :
-            ptr_{tmp.ptr_}
+            storage_{tmp.storage_}
         {
-            static_cast<Metadata*>(ptr_)->num_owners.fetch_add(1, std::memory_order_relaxed);
+            storage_->num_owners.fetch_add(1, std::memory_order_relaxed);
         }
 
         SharedPreHashedString& operator=(const SharedPreHashedString& src) noexcept
@@ -121,14 +101,15 @@ namespace osc
 
         ~SharedPreHashedString() noexcept
         {
-            if (static_cast<Metadata*>(ptr_)->num_owners.fetch_sub(1, std::memory_order_relaxed) == 1) {
-                ::operator delete(ptr_, std::align_val_t{alignof(Metadata)});
+            if (storage_->num_owners.fetch_sub(1, std::memory_order_release) == 1) {
+                std::atomic_thread_fence(std::memory_order_acquire);
+                delete storage_;
             }
         }
 
         friend void swap(SharedPreHashedString& a, SharedPreHashedString& b) noexcept
         {
-            std::swap(a.ptr_, b.ptr_);
+            std::swap(a.storage_, b.storage_);
         }
 
         // returns a reference to the character at specified position `pos` if `pos <= size()`. Bounds checking
@@ -161,8 +142,7 @@ namespace osc
         // returns a pointer to the underlying array serving as character storage
         const value_type* data() const
         {
-            static_assert(sizeof(value_type) == 1);
-            return static_cast<value_type*>(ptr_) + sizeof(Metadata);
+            return storage_->data();
         }
 
         // returns a pointer to a null-terminated character array with data equivalent to those stored in the string.
@@ -221,12 +201,12 @@ namespace osc
         // returns the number of characters in the string
         size_type size() const
         {
-            return static_cast<const Metadata*>(ptr_)->size;
+            return storage_->size;
         }
 
         friend constexpr bool operator==(const SharedPreHashedString& lhs, const SharedPreHashedString& rhs)
         {
-            return lhs.ptr_ == rhs.ptr_ or std::string_view{lhs} == std::string_view{rhs};
+            return lhs.storage_ == rhs.storage_ or std::string_view{lhs} == std::string_view{rhs};
         }
 
         template<typename StringViewLike>
@@ -245,7 +225,7 @@ namespace osc
 
         friend constexpr auto operator<=>(const SharedPreHashedString& lhs, const SharedPreHashedString& rhs)
         {
-            if (lhs.ptr_ == rhs.ptr_) {
+            if (lhs.storage_ == rhs.storage_) {
                 return std::strong_ordering::equal;
             }
             return std::string_view{lhs} <=> std::string_view{rhs};
@@ -276,33 +256,87 @@ namespace osc
         // `std::memory_order_relaxed` operation to load the count)
         long use_count() const noexcept
         {
-            return static_cast<long>(static_cast<Metadata*>(ptr_)->num_owners.load(std::memory_order_relaxed));
+            return static_cast<long>(storage_->num_owners.load(std::memory_order_relaxed));
+        }
+
+        size_t hash() const
+        {
+            return storage_->hash;
         }
     private:
-        friend struct std::hash<SharedPreHashedString>;
-
         static const SharedPreHashedString& static_default_instance()
         {
             static SharedPreHashedString s_default_instance{std::string_view{}};
             return s_default_instance;
         }
 
-        // internal data structure that's placed at the front of the dynamically-allocated
-        // allocated memory block
-        struct Metadata final {
+        // Internal storage for this string type. Stores metadata (size, hash, etc.)
+        // immediately followed by the string's bytes.
+        class Storage final {
+        public:
+            static Storage* create(std::string_view str)
+            {
+                // Allocate memory
+                void* mem = ::operator new(storage_allocation_size(str.size()), std::align_val_t{alignof(Storage)});
 
-            explicit Metadata(std::string_view str) noexcept :
-                size{str.size()},
-                hash{std::hash<std::string_view>{}(str)}
-            {}
+                // Initialize header at the front
+                std::unique_ptr<Storage> storage{::new (mem) Storage{str}};
+
+                // Copy string data after it.
+                std::uninitialized_copy_n(str.data(), str.size(), storage->data());
+
+                // Initialize NUL terminator after string data.
+                storage->data()[str.size()] = value_type{};
+
+                return storage.release();
+            }
+
+            Storage(const Storage&) = delete;
+            Storage(Storage&&) noexcept = delete;
+            Storage& operator=(const Storage&) = delete;
+            Storage& operator=(Storage&&) noexcept = delete;
+            ~Storage() noexcept = default;
+
+            void operator delete(Storage* ptr, std::destroying_delete_t)
+            {
+                ptr->~Storage();
+                ::operator delete(
+                    static_cast<void*>(ptr),
+                    storage_allocation_size(ptr->size),
+                    std::align_val_t{alignof(Storage)}
+                );
+            }
+
+            value_type* data() noexcept
+            {
+                static_assert(alignof(Storage) >= alignof(value_type));
+                return reinterpret_cast<value_type*>(this + 1);
+            }
+            const value_type* data() const noexcept
+            {
+                static_assert(alignof(Storage) >= alignof(value_type));
+                return reinterpret_cast<const value_type*>(this + 1);
+            }
 
             size_t size;
             size_t hash;
             std::atomic<size_t> num_owners = 1;
+
+        private:
+            static constexpr size_t storage_allocation_size(size_t string_len)
+            {
+                // Header + data + NUL
+                static_assert(alignof(Storage) >= alignof(value_type));
+                return sizeof(Storage) + (sizeof(value_type)*(string_len + 1));
+            }
+
+            explicit Storage(std::string_view str) noexcept :
+                size{str.size()},
+                hash{std::hash<std::string_view>{}(str)}
+            {}
         };
 
-        // `StringNameData`, followed by characters, followed by NUL terminator
-        void* ptr_ = nullptr;
+        Storage* storage_;
     };
 }
 
@@ -310,6 +344,6 @@ template<>
 struct std::hash<osc::SharedPreHashedString> final {
     size_t operator()(const osc::SharedPreHashedString& str) const noexcept
     {
-        return static_cast<const osc::SharedPreHashedString::Metadata*>(str.ptr_)->hash;
+        return str.hash();
     }
 };
